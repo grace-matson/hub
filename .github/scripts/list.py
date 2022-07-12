26a61781ec6b5ab1376202fa7fbb8d55e7978823
grace-matson <gracematson@google.com>
2022-07-12 04:30:10 +0000

    Edit list.py

--- .github/scripts/list.py
+++ .github/scripts/list.py
@@ -9,15 +9,15 @@ specfiles = []
 for file in list:
   if(file.split('/')[-1]=="spec.json"):
     specfiles.append(file)
-
+print(specfiles)
 if(len(specfiles)==0):
   sys.exit(0)
-
+utilities.run_shell_command('ls')
 os.chdir('./packager/')
 utilities.run_shell_command('mvn clean package')
 os.chdir('../')
 utilities.run_shell_command('java -cp "packager/target/lib/*:packager/target/*" io.cdap.hub.Tool build')
-
+utilities.run_shell_command('ls')
 f = open("./packages.json", "r")
 l = json.loads(f.read())
 print(l)
